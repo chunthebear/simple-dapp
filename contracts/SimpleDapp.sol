6daf688b8fe7fd9f147949adc3e4b61705f06893
chunthebear <zyc5026@hotmail.com>
2019-01-15 14:51:28 -0800

    Updated with newest version of Solidity 0.5.0

--- contracts/SimpleDapp.sol
+++ contracts/SimpleDapp.sol
@@ -1,55 +1,49 @@
-pragma solidity ^0.4.4;
-contract SimpleDapp{
-	address Admin;
+pragma solidity ^0.5.0;
+contract simpleDapp{
+    address Admin;
 
-	/*mapping(bytes32=>notarizedImage) notarizedImage;
-	bytes32[] imagesByNotaryHash;
-	*/
-	mapping(address=>User) Users;
-	address[] usersByAddress;
+    /*mapping(bytes32=>notarizedImage) notarizedImage;
+    bytes32[] imagesByNotaryHash;
+    */
+    mapping(address=>User) users;
+    address[] usersByAddress;
 
-	/*struct notarizedImage{
-		string imageURL;
-		uint timeStamp;
-	}*/
+    /*struct notarizedImage{
+        string imageURL;
+        uint timeStamp;
+    }*/
 
-	struct User{
-		string name;
-		bytes32 city;
-		bytes32 country;
-		//bytes32[] myImages;
-	}
+    struct User{
+        string name;
+        string city;
+        string country;
+        //bytes32[] myImages;
+    }
 
-	function SimpleDapp() payable {  //CONSTRUCTOR gets called ONCE only when contract is first deployed
-	    Admin = msg.sender;  // just set the admin, so they can remove bad users or images if needed, but nobody else can
-	 }
+    constructor() payable public {  //CONSTRUCTOR gets called ONCE only when contract is first deployed
+        Admin = msg.sender;  // just set the admin, so they can remove bad users or images if needed, but nobody else can
+    }
 
-	modifier onlyAdmin() {
-    if (msg.sender != Admin)
-      throw;
-    _; // Do not forget the "_;"! It will be replaced by the actual function body when the modifier is used.
-  }
+    modifier onlyAdmin() {
+        require(Admin==msg.sender, "Sender is not admin");
+        _; // Do not forget the "_;"! It will be replaced by the actual function body when the modifier is used.
+    }
 
-	function register(string name, bytes32 city, bytes32 country) returns(bool success){
-		address newAddress = msg.sender;
+    function register(string memory name, string memory city, string memory country) public{
+        address newAddress = msg.sender;
 
-		if(bytes(Users[newAddress].name).length==0 && bytes(name).length!=0){
-			Users[newAddress].name = name;
-			Users[newAddress].city = city;
-			Users[newAddress].country = country;
-			usersByAddress.push(newAddress);
-			return true;
-		} else{
-			return false;
-		}
-	}
+        users[newAddress].name = name;
+        users[newAddress].city = city;
+        users[newAddress].country = country;
+        usersByAddress.push(newAddress);
+    }
 
-	function getUsers() constant returns (address[]){
-		return usersByAddress;
-	}
+    function getUsers() public view returns (address[] memory){
+        return usersByAddress;
+    }
 
-	function getUser (address userAddress) constant returns (string, bytes32, bytes32){
-		return (Users[userAddress].name, Users[userAddress].city, Users[userAddress].country);
-	}
+    function getUser (address userAddress) public view returns (string memory, string memory , string memory ){
+        return (users[userAddress].name, users[userAddress].city, users[userAddress].country);
+    }
 
 }
